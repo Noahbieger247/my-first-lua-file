6853b79414593048084add890028db2d131bfc2c
Noahbieger247 <130004360+Noahbieger247@users.noreply.github.com>
2023-04-05 18:34:42 -0400

    created main file

---
+++ main.lua
@@ -0,0 +1,47 @@
+--[[optional_function_scope function function_name( argument1, argument2, argument3........, 
+argumentn)
+function_body
+return result_params_comma_separated
+end
+--]]
+local num1 
+local num2
+local cond
+print("do you want to run \ny/n")
+cond =io.read("*line")
+while(cond == "y")
+do
+    
+    
+
+
+
+local function joe_mama(so_fat,so_stupid) 
+       --[[for init,max/min value, increment
+do
+   statement(s)
+end--]]
+    for i = 1, so_fat, 1
+    do
+        
+        for i = 1, so_stupid, 1
+        do
+            io.write("x")
+        end
+        io.write("\n")
+    end
+    
+    
+    
+    
+    
+    
+end
+
+print("put in two numbers")
+num1,num2 = io.read("*number","*number")
+print(" num1 is " .. num1 .. " num2 is " .. num2)
+joe_mama(num1,num2)
+print("do you want to run again \ny/n")
+cond =io.read("*line")
+end
